acad683d2ecafa00f128192cead3cb8511b431da
JRyanHenson <hensojr@gmail.com>
2017-08-01 11:34:32 -0600

    added images and text

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge" />
+		<meta name=="viewport" content="width=device-width, initial-scale=1" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<title>Challenges</title>
+	</head>
+	<body>
+			<h1 class="text-center">This is our challenge</h1>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="https://www.fillmurray.com/200/300">
+					</div>
+					<div class="col-md-4">
+						<img src="https://www.fillmurray.com/g/200/300">
+					</div>
+					<div class="col-md-4">
+						<img src="http://www.stevensegallery.com/200/300">
+					</div><!--column-->
+				</div><!--row-->
+				<div class="row">
+					<div class="col-md-4">
+						<p>Tiramisu dessert cookie gummi bears icing biscuit dragée donut gummi bears. Dragée donut icing pastry marzipan cookie jelly beans. Tiramisu sesame snaps sweet roll tootsie roll jelly beans chocolate cake dessert chocolate bar apple pie.
+						Cupcake soufflé gummi bears lemon drops carrot cake candy canes ice cream jelly lollipop. Cookie topping apple pie. Icing jelly toffee.
+						Cake chupa chups icing dragée marshmallow jelly-o. Lemon drops chocolate cake tart brownie sesame snaps topping soufflé gummies dessert. Marzipan toffee cupcake ice cream pastry ice cream tiramisu.
+						Tart soufflé donut biscuit biscuit brownie. Dessert tart jelly pastry chocolate bar sweet. Macaroon topping bear claw pudding dragée chupa chups gingerbread chupa chups caramels. Dessert caramels candy canes.
+							Dessert bonbon topping gingerbread. Lemon drops pudding jelly pie soufflé cupcake. Muffin pastry icing.</p>
+					</div>
+					<div class="col-md-4">
+						<p>TEst sugar, medium foam java variety cortado fair trade strong. In instant, black sweet fair trade aroma id redeye chicory. Strong dark, decaffeinated grinder a turkish flavour roast to go. Coffee galão, robusta barista caramelization mocha grinder. Cinnamon single shot to go caramelization pumpkin spice, turkish percolator grounds redeye americano.
+
+							Instant, organic café au lait crema, and carajillo cream flavour crema. Flavour cinnamon, eu, extraction, affogato half and half milk carajillo cream crema. Whipped redeye, mocha, extra, cup cultivar, caramelization pumpkin spice body robust americano. Percolator cultivar sweet, so beans iced acerbic affogato grinder. Chicory rich wings single shot et galão est irish.
+
+							Froth skinny, strong that decaffeinated cream roast irish strong. To go, plunger pot, wings frappuccino, foam, aftertaste americano pumpkin spice steamed aged brewed. Irish decaffeinated siphon mocha, at, sweet, flavour macchiato brewed roast arabica plunger pot. Est viennese skinny, ristretto bar ristretto at sit wings extra foam. To go beans café au lait arabica french press steamed mazagran steamed shop.
+
+							Foam crema siphon to go aftertaste rich that black java. Lungo cappuccino, dripper doppio cinnamon est cup spoon wings chicory cinnamon viennese. Cultivar trifecta plunger pot, pumpkin spice eu extraction french press percolator spoon pumpkin spice. Body milk, sugar cappuccino espresso doppio grinder roast aged. Mocha lungo crema con panna organic blue mountain milk frappuccino siphon.</p>
+					</div>
+					<div class="col-md-4">
+						<p>TVicious and viscous, this menacing brew pours opaque black with a creamy maduro-colored head. Its aroma offers seductive whiskey, chewy red wine, dark fruit and lavish tobacco. Berserker Imperial Stout invades your taste buds with in-your-face flavor. Weighing in at almost 13% alcohol by volume, Berserker is completely out-of-control. Give it a good fight. Berserker Imperial Stout was aged in both red wine and whiskey barrels.
+
+						</p>
+					</div>
+				</div>
+
+			</div><!--container-->
+
+
+	</body>
+</html>
